f743fbbd77e9c97678f45cdab5e6ebf3dff94bef
Kaíque Jacob <kaiquejacob975@gmail.com>
2026-04-21 00:43:43 -0300

    Arrays01

---
+++ Java/Arrays01.java
@@ -0,0 +1,13 @@
+public class Arrays01 {
+    static void main(String[] args) {
+        int [] idades = new int[3];             // índice vai de 0 a 2
+        idades[0] = 21;
+        idades[1] = 15;
+        idades[2] = 11;
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+        System.out.println(idades[2]);
+        System.out.println(idades[2]);
+    }
+}
