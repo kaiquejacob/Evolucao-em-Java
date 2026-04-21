a8125247329d2eb4c4c6e3f6e40823b42ef531f3
Kaíque Jacob <kaiquejacob975@gmail.com>
2026-04-21 00:38:09 -0300

    Tipos primitivos

---
+++ Java/Tipos_primitivos.java
@@ -0,0 +1,30 @@
+public class Tipos_primitivos {
+    static void main(String[] args){
+        //int, double, float, char, byte, short, long, boolean
+        int idade = 10;
+        long numero_grande = 1000000;
+        float salario = 2000;
+        double salarioG = 2500000;
+        byte idadeByte = 127;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'M';
+        System.out.println(idade);
+        System.out.println("A idade é " + idade + " anos");
+        System.out.println(numero_grande);
+        System.out.println(salario);
+        System.out.println(salarioG);
+        System.out.println(idadeByte);
+        System.out.println(idadeShort);
+        System.out.println(verdadeiro);
+        System.out.println(falso);
+        System.out.println(caractere);
+
+        //String
+
+        String nome = "Kaíque";
+        System.out.println(nome);
+        System.out.println("Meu nome é "+nome);
+    }
+}
