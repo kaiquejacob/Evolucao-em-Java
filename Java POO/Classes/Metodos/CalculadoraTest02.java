54835e8409a86c1d7df062d7e346fda0e5007a5c
Kaíque Jacob <kaiquejacob975@gmail.com>
2026-04-25 23:34:47 -0300

    Metodos

---
+++ Java POO/Classes/Metodos/CalculadoraTest02.java
@@ -0,0 +1,6 @@
+public class CalculadoraTest02 {
+    static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.multiplicaDoisNumeros(10, 20);
+    }
+}
---
+++ Java POO/Classes/Metodos/CalculadoraTest03.java
@@ -0,0 +1,7 @@
+public class CalculadoraTest03 {
+    static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        double resultado = calculadora.divideDoisNumeros(20, 2);
+        System.out.println(resultado);
+    }
+}
