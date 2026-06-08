ad436556d4066826f8a8717fd37fcf13634de85b
Kaíque Jacob <kaiquejacob975@gmail.com>
2026-06-08 17:58:13 -0300

    Enumeração

---
+++ Java POO pt03/TipoCliente.java
@@ -0,0 +1,13 @@
+public enum TipoCliente {
+    PESSOA_FISICA(1, "Pessoa Física"),
+    PESSOA_JURIDICA(2, "Pessoa Jurídica");
+
+    public final int VALOR;
+    public final String nomeRelatorio;
+
+    TipoCliente(int valor, String nomeRelatorio) {
+        this.VALOR = valor;
+        this.nomeRelatorio = nomeRelatorio;
+    }
+
+}
