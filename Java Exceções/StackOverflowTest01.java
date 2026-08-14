03f3de0459c1de4d763079e704d9608b450ca67b
Kaíque Jacob <kaiquejacob975@gmail.com>
2026-08-14 17:35:15 -0300

    Error Test

---
+++ Java Exceções/StackOverflowTest01.java
@@ -0,0 +1,9 @@
+public class StackOverflowTest01 {
+    static void main(String[] args) {
+        //recursividade();
+    }
+
+    public static void recursividade(){
+        recursividade();
+    }
+}
