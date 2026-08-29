f4fc67a18dca51c98e5735fa0db737182a843e7f
Kaíque Jacob <kaiquejacob975@gmail.com>
2026-08-29 14:44:01 -0300

    .

---
+++ Java Classes Utilitárias/Serialização/Aluno.java
@@ -0,0 +1,82 @@
+import java.io.*;
+
+public class Aluno implements Serializable {
+    @Serial
+    private static final long serialVersionUID = -4670722371063044733L;
+    private Long id;
+    private String nome;
+    private transient String password;
+    private static String nomeEscola = "Dev Dojo";
+    private transient Turma turma;
+
+    public Aluno(Long id, String nome, String password) {
+        this.id = id;
+        this.nome = nome;
+        this.password = password;
+    }
+
+    @Serial
+    private void writeObject(ObjectOutputStream oos){
+        try {
+            oos.defaultWriteObject();
+            oos.writeUTF(turma.getNome());
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Serial
+    private void readObject(ObjectInputStream ois){
+        try {
+            ois.defaultReadObject();
+            String nomeTurma = ois.readUTF();
+            turma = new Turma(nomeTurma);
+
+        }catch (IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", password='" + password + '\'' +
+                ", nomeEscola='" + nomeEscola + '\'' +
+                ", Turma='" + turma + '\'' +
+                '}';
+    }
+
+    public Turma getTurma() {
+        return turma;
+    }
+
+    public void setTurma(Turma turma) {
+        this.turma = turma;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ Java Classes Utilitárias/Serialização/SerializacaoTest01.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class SerializacaoTest01 {
+    static void main(String[] args) {
+        Aluno aluno = new Aluno(1L, "Sandro Luiz", "123456");
+        Turma turma = new Turma("Maratona Java");
+        aluno.setTurma(turma);
+
+        serializar(aluno);
+        deserializar();
+    }
+
+    private static void serializar(Aluno aluno){
+        Path path = Paths.get("pasta/aluno.ser");
+        try (ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(path))){
+            oos.writeObject(aluno);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void deserializar(){
+        Path path = Paths.get("pasta/aluno.ser");
+        try (ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(path))){
+            Aluno aluno = (Aluno) ois.readObject();
+            System.out.println(aluno);
+        }catch (IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+}
