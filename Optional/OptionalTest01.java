25aa685adcec079d08994c762278c45b277e0be9
Kaíque Jacob <kaiquejacob975@gmail.com>
2026-09-11 22:16:58 -0300

    .

---
+++ Optional/OptionalTest01.java
@@ -0,0 +1,30 @@
+import java.util.List;
+import java.util.Optional;
+
+public class OptionalTest01 {
+    static void main(String[] args) {
+        Optional<String> o1 = Optional.of("Aha unhu o DevDojo é foda");
+        Optional<String> o2 = Optional.ofNullable(null);
+        Optional<String> o3 = Optional.empty();
+
+        System.out.println(o1);
+        System.out.println(o2);
+        System.out.println(o3);
+        System.out.println("--------------");
+
+        Optional<String> nameOptional = findName("William");
+        String empty = nameOptional.orElse("EMPTY");
+        System.out.println(empty);
+        nameOptional.ifPresent(s -> System.out.println(s.toUpperCase()));
+
+    }
+
+    private static Optional<String> findName(String name){
+        List<String> list = List.of("William", "DevDojo");
+        int i = list.indexOf(name);
+        if(i != 1) {
+            return Optional.of(list.get(i));
+        }
+        return Optional.empty();
+    }
+}
---
+++ Optional/OptionalTest02.java
@@ -0,0 +1,15 @@
+import java.util.Optional;
+
+public class OptionalTest02 {
+    static void main(String[] args) {
+        Optional<Manga> mangaByTitle = MangaRepository.findByTitle("Boku no hero");
+        mangaByTitle.ifPresent(m -> m.setTitle("Boku no hero 2"));
+        System.out.println(mangaByTitle);
+
+        Manga mangaById = MangaRepository.findById(2).orElseThrow(IllegalArgumentException::new);
+        System.out.println(mangaById);
+
+        Manga newManga = MangaRepository.findByTitle("Drifters").orElseGet(() -> new Manga(3, "Drifters", 20));
+        System.out.println(newManga);
+    }
+}
